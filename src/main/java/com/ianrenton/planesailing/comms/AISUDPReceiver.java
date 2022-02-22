06f999faf0d6515b75871ab4f513c8ebecb7bf49
Ian Renton <ian@ianrenton.com>
2022-02-22 18:05:15 +0000

    Improve exception handling and null checking in AISUDPReceiver. Should fix #17.

--- src/main/java/com/ianrenton/planesailing/comms/AISUDPReceiver.java
+++ src/main/java/com/ianrenton/planesailing/comms/AISUDPReceiver.java
@@ -86,171 +86,186 @@ public class AISUDPReceiver extends Client {
 	 * @param m
 	 */
 	private void handle(AISMessage m) {
-		int mmsi = m.getSourceMmsi().getMMSI();
-		String mmsiString = String.valueOf(mmsi);
-		
-		// If this is a new track, add it to the track table
-		if (!trackTable.containsKey(mmsiString)) {
-			AISTrack newS = new AISTrack(mmsi);
-
-			// If we have a name for this ship in our cache cache of past data,
-			// set the name immediately.
-			if (newS.getName() == null && trackTable.getAISNameCache().containsKey(mmsi)) {
-				newS.setName(trackTable.getAISNameCache().get(mmsi));
+		try {
+			int mmsi = m.getSourceMmsi().getMMSI();
+			String mmsiString = String.valueOf(mmsi);
+			
+			// If this is a new track, add it to the track table
+			if (!trackTable.containsKey(mmsiString)) {
+				AISTrack newS = new AISTrack(mmsi);
+	
+				// If we have a name for this ship in our cache cache of past data,
+				// set the name immediately.
+				if (newS.getName() == null && trackTable.getAISNameCache().containsKey(mmsi)) {
+					newS.setName(trackTable.getAISNameCache().get(mmsi));
+				}
+	
+				trackTable.put(mmsiString, newS);
 			}
-
-			trackTable.put(mmsiString, newS);
-		}
-		
-		// Extract the data and update the track
-		AISTrack s = (AISTrack) trackTable.get(mmsiString);
-		switch (m.getMessageType()) {
-		case AidToNavigationReport:
-			AidToNavigationReport m2 = (AidToNavigationReport) m;
-			s.setAtoN(true);
-			s.setName(m2.getName().replaceAll("_", " ").replaceAll("@", " ").trim());
-			s.addPosition(m2.getLatitude(), m2.getLongitude());
-			s.setTrackType(TrackType.AIS_ATON);
-			s.setFixed(true);
-			s.updateMetadataTime();
-			break;
 			
-		case BaseStationReport:
-			BaseStationReport m3 = (BaseStationReport) m;
-			s.setShoreStation(true);
-			s.addPosition(m3.getLatitude(), m3.getLongitude());
-			s.setTrackType(TrackType.AIS_SHORE_STATION);
-			s.setFixed(true);
-			s.updateMetadataTime();
-			break;
-			
-		case ClassBCSStaticDataReport:
-			ClassBCSStaticDataReport m4 = (ClassBCSStaticDataReport) m;
-			if (m4.getShipName() != null) {
-				String name = m4.getShipName().replaceAll("_", " ").replaceAll("@", " ").trim(); 
-				s.setName(name);
-				trackTable.getAISNameCache().put(mmsi, name);
+			// Extract the data and update the track
+			AISTrack s = (AISTrack) trackTable.get(mmsiString);
+			switch (m.getMessageType()) {
+			case AidToNavigationReport:
+				AidToNavigationReport m2 = (AidToNavigationReport) m;
+				s.setAtoN(true);
+				if (m2.getName() != null) {
+					String name = m2.getName().replaceAll("_", " ").replaceAll("@", " ").trim();
+					s.setName(name);
+					trackTable.getAISNameCache().put(mmsi, name);
+				}
+				s.addPosition(m2.getLatitude(), m2.getLongitude());
+				s.setTrackType(TrackType.AIS_ATON);
+				s.setFixed(true);
+				s.updateMetadataTime();
+				break;
+				
+			case BaseStationReport:
+				BaseStationReport m3 = (BaseStationReport) m;
+				s.setShoreStation(true);
+				s.addPosition(m3.getLatitude(), m3.getLongitude());
+				s.setTrackType(TrackType.AIS_SHORE_STATION);
+				s.setFixed(true);
+				s.updateMetadataTime();
+				break;
+				
+			case ClassBCSStaticDataReport:
+				ClassBCSStaticDataReport m4 = (ClassBCSStaticDataReport) m;
+				if (m4.getShipName() != null) {
+					String name = m4.getShipName().replaceAll("_", " ").replaceAll("@", " ").trim(); 
+					s.setName(name);
+					trackTable.getAISNameCache().put(mmsi, name);
+				}
+				if (m4.getCallsign() != null) {
+					s.setCallsign(m4.getCallsign().trim());
+				}
+				s.setShipType(m4.getShipType());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case ExtendedClassBEquipmentPositionReport:
+				ExtendedClassBEquipmentPositionReport m5 = (ExtendedClassBEquipmentPositionReport) m;
+				if (m5.getShipName() != null) {
+					String name = m5.getShipName().replaceAll("_", " ").replaceAll("@", " ").trim(); 
+					s.setName(name);
+					trackTable.getAISNameCache().put(mmsi, name);
+				}
+				
+				s.addPosition(m5.getLatitude(), m5.getLongitude());
+				if (m5.getCourseOverGround() != 511) {
+					s.setCourse(m5.getCourseOverGround().doubleValue());
+				}
+				if (m5.getTrueHeading() != 0 && m5.getTrueHeading() != 511) {
+					s.setHeading(m5.getTrueHeading().doubleValue());
+				}
+				s.setSpeed(m5.getSpeedOverGround().doubleValue());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case LongRangeBroadcastMessage:
+				LongRangeBroadcastMessage m6 = (LongRangeBroadcastMessage) m;
+				s.addPosition(m6.getLatitude(), m6.getLongitude());
+				if (m6.getCourseOverGround() != 511) {
+					s.setCourse(m6.getCourseOverGround().doubleValue());
+				}
+				s.setSpeed(m6.getSpeedOverGround().doubleValue());
+				s.setNavStatus(m6.getNavigationalStatus());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case PositionReportClassAAssignedSchedule:
+				PositionReportClassAAssignedSchedule m7 = (PositionReportClassAAssignedSchedule) m;
+				s.addPosition(m7.getLatitude(), m7.getLongitude());
+				if (m7.getCourseOverGround() != 511) {
+					s.setCourse(m7.getCourseOverGround().doubleValue());
+				}
+				if (m7.getTrueHeading() != 0 && m7.getTrueHeading() != 511) {
+					s.setHeading(m7.getTrueHeading().doubleValue());
+				}
+				s.setSpeed(m7.getSpeedOverGround().doubleValue());
+				s.setNavStatus(m7.getNavigationStatus());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case PositionReportClassAResponseToInterrogation:
+				PositionReportClassAResponseToInterrogation m8 = (PositionReportClassAResponseToInterrogation) m;
+				s.addPosition(m8.getLatitude(), m8.getLongitude());
+				if (m8.getCourseOverGround() != 511) {
+					s.setCourse(m8.getCourseOverGround().doubleValue());
+				}
+				if (m8.getTrueHeading() != 0 && m8.getTrueHeading() != 511) {
+					s.setHeading(m8.getTrueHeading().doubleValue());
+				}
+				s.setSpeed(m8.getSpeedOverGround().doubleValue());
+				s.setNavStatus(m8.getNavigationStatus());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case PositionReportClassAScheduled:
+				PositionReportClassAScheduled m9 = (PositionReportClassAScheduled) m;
+				s.addPosition(m9.getLatitude(), m9.getLongitude());
+				if (m9.getCourseOverGround() != 511) {
+					s.setCourse(m9.getCourseOverGround().doubleValue());
+				}
+				if (m9.getTrueHeading() != 0 && m9.getTrueHeading() != 511) {
+					s.setHeading(m9.getTrueHeading().doubleValue());
+				}
+				s.setSpeed(m9.getSpeedOverGround().doubleValue());
+				s.setNavStatus(m9.getNavigationStatus());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case ShipAndVoyageRelatedData:
+				ShipAndVoyageData m10 = (ShipAndVoyageData) m;
+				if (m10.getShipName() != null) {
+					String name10 = m10.getShipName().replaceAll("_", " ").replaceAll("@", " ").trim(); 
+					s.setName(name10);
+					trackTable.getAISNameCache().put(mmsi, name10);
+				}
+				if (m10.getCallsign() != null) {
+					s.setCallsign(m10.getCallsign().trim());
+				}
+				s.setShipType(m10.getShipType());
+				s.setDestination(m10.getDestination());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case StandardClassBCSPositionReport:
+				StandardClassBCSPositionReport m11 = (StandardClassBCSPositionReport) m;
+				s.addPosition(m11.getLatitude(), m11.getLongitude());
+				if (m11.getCourseOverGround() != 511) {
+					s.setCourse(m11.getCourseOverGround().doubleValue());
+				}
+				if (m11.getTrueHeading() != 0 && m11.getTrueHeading() != 511) {
+					s.setHeading(m11.getTrueHeading().doubleValue());
+				}
+				s.setSpeed(m11.getSpeedOverGround().doubleValue());
+				s.setTrackType(TrackType.SHIP);
+				s.updateMetadataTime();
+				break;
+				
+			case UTCAndDateResponse:
+				UTCAndDateResponse m12 = (UTCAndDateResponse) m;
+				s.addPosition(m12.getLatitude(), m12.getLongitude());
+				break;
+				
+			case StandardSARAircraftPositionReport:
+				// This aircraft will have ADS-B as well, so don't worry about its AIS track
+				break;
+				
+			default:
+				// Nothing useful we can do with this type
+				break;
 			}
-			s.setCallsign(m4.getCallsign().trim());
-			s.setShipType(m4.getShipType());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case ExtendedClassBEquipmentPositionReport:
-			ExtendedClassBEquipmentPositionReport m5 = (ExtendedClassBEquipmentPositionReport) m;
-			String name = m5.getShipName().replaceAll("_", " ").replaceAll("@", " ").trim(); 
-			s.setName(name);
-			trackTable.getAISNameCache().put(mmsi, name);
-			
-			s.addPosition(m5.getLatitude(), m5.getLongitude());
-			if (m5.getCourseOverGround() != 511) {
-				s.setCourse(m5.getCourseOverGround().doubleValue());
-			}
-			if (m5.getTrueHeading() != 0 && m5.getTrueHeading() != 511) {
-				s.setHeading(m5.getTrueHeading().doubleValue());
-			}
-			s.setSpeed(m5.getSpeedOverGround().doubleValue());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case LongRangeBroadcastMessage:
-			LongRangeBroadcastMessage m6 = (LongRangeBroadcastMessage) m;
-			s.addPosition(m6.getLatitude(), m6.getLongitude());
-			if (m6.getCourseOverGround() != 511) {
-				s.setCourse(m6.getCourseOverGround().doubleValue());
-			}
-			s.setSpeed(m6.getSpeedOverGround().doubleValue());
-			s.setNavStatus(m6.getNavigationalStatus());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case PositionReportClassAAssignedSchedule:
-			PositionReportClassAAssignedSchedule m7 = (PositionReportClassAAssignedSchedule) m;
-			s.addPosition(m7.getLatitude(), m7.getLongitude());
-			if (m7.getCourseOverGround() != 511) {
-				s.setCourse(m7.getCourseOverGround().doubleValue());
-			}
-			if (m7.getTrueHeading() != 0 && m7.getTrueHeading() != 511) {
-				s.setHeading(m7.getTrueHeading().doubleValue());
-			}
-			s.setSpeed(m7.getSpeedOverGround().doubleValue());
-			s.setNavStatus(m7.getNavigationStatus());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case PositionReportClassAResponseToInterrogation:
-			PositionReportClassAResponseToInterrogation m8 = (PositionReportClassAResponseToInterrogation) m;
-			s.addPosition(m8.getLatitude(), m8.getLongitude());
-			if (m8.getCourseOverGround() != 511) {
-				s.setCourse(m8.getCourseOverGround().doubleValue());
-			}
-			if (m8.getTrueHeading() != 0 && m8.getTrueHeading() != 511) {
-				s.setHeading(m8.getTrueHeading().doubleValue());
-			}
-			s.setSpeed(m8.getSpeedOverGround().doubleValue());
-			s.setNavStatus(m8.getNavigationStatus());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case PositionReportClassAScheduled:
-			PositionReportClassAScheduled m9 = (PositionReportClassAScheduled) m;
-			s.addPosition(m9.getLatitude(), m9.getLongitude());
-			if (m9.getCourseOverGround() != 511) {
-				s.setCourse(m9.getCourseOverGround().doubleValue());
-			}
-			if (m9.getTrueHeading() != 0 && m9.getTrueHeading() != 511) {
-				s.setHeading(m9.getTrueHeading().doubleValue());
-			}
-			s.setSpeed(m9.getSpeedOverGround().doubleValue());
-			s.setNavStatus(m9.getNavigationStatus());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case ShipAndVoyageRelatedData:
-			ShipAndVoyageData m10 = (ShipAndVoyageData) m;
-			String name10 = m10.getShipName().replaceAll("_", " ").replaceAll("@", " ").trim(); 
-			s.setName(name10);
-			trackTable.getAISNameCache().put(mmsi, name10);
-			
-			s.setCallsign(m10.getCallsign().trim());
-			s.setShipType(m10.getShipType());
-			s.setDestination(m10.getDestination());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case StandardClassBCSPositionReport:
-			StandardClassBCSPositionReport m11 = (StandardClassBCSPositionReport) m;
-			s.addPosition(m11.getLatitude(), m11.getLongitude());
-			if (m11.getCourseOverGround() != 511) {
-				s.setCourse(m11.getCourseOverGround().doubleValue());
-			}
-			if (m11.getTrueHeading() != 0 && m11.getTrueHeading() != 511) {
-				s.setHeading(m11.getTrueHeading().doubleValue());
-			}
-			s.setSpeed(m11.getSpeedOverGround().doubleValue());
-			s.setTrackType(TrackType.SHIP);
-			s.updateMetadataTime();
-			break;
-			
-		case UTCAndDateResponse:
-			UTCAndDateResponse m12 = (UTCAndDateResponse) m;
-			s.addPosition(m12.getLatitude(), m12.getLongitude());
-			break;
-			
-		case StandardSARAircraftPositionReport:
-			// This aircraft will have ADS-B as well, so don't worry about its AIS track
-			break;
-			
-		default:
-			// Nothing useful we can do with this type
-			break;
+		} catch (Exception ex) {
+			LOGGER.error("Exception handling AIS data, catching exception so handling can continue.", ex);
 		}
 	}
 
